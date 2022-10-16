622f1f3ef0c83ee708104257f82de3ea1860aa2e
Mehdi Zaidi <mehdi.zaidi.etu@univ-lille.fr>
2022-10-16 11:29:23 +0200

    tests passing

---
+++ src/test/java/tps/tpcsv/testTest.java
@@ -0,0 +1,14 @@
+package test.java.tps.tpcsv;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class testTest {
+
+	@Test
+	void test() {
+		assertEquals("test", "test");
+	}
+
+}
