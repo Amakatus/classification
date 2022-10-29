b5830653e110aba4777545985bfde9ba822915df
Mehdi Zaidi <mehdi.zaidi.etu@univ-lille.fr>
2022-10-29 23:24:09 +0200

    more tests to satisfy jacoco

--- src/test/java/app/algorithm/AlgorithmFactoryTest.java
+++ src/test/java/app/algorithm/AlgorithmFactoryTest.java
@@ -1,6 +1,7 @@
 package app.algorithm;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.IOException;
@@ -21,10 +22,12 @@ class AlgorithmFactoryTest {
 			Dataset<IrisData> reference = new Dataset<IrisData>("ReferenceDS", CSVUtils.loadIrisCSV());
 			IrisData toClassify = new IrisData();
 			Dataset<IrisData> working = new Dataset<IrisData>("WorkingDS", Arrays.asList(toClassify), DatasetType.WORKING);
-			boolean algoDone = AlgorithmFactory.createAlgorithm(working, reference, 5);
-			assertTrue(algoDone);
+			assertTrue(AlgorithmFactory.createAlgorithm(working, reference, 5));
 			assertEquals(1, working.getAlgorithms().size());
 			assertEquals(5, working.getAlgorithms().get(0).getK());
+			assertFalse(AlgorithmFactory.createAlgorithm(working, working, 5));
+			assertFalse(AlgorithmFactory.createAlgorithm(reference, reference, 5));
+			assertFalse(AlgorithmFactory.createAlgorithm(reference, working, 5));
 		} catch (IOException e) { e.printStackTrace(); }
 	}
 
