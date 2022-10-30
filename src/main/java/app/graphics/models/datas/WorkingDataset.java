7954b32126c39df2e95f21753db189aee8d00a60
Mehdi Zaidi <mehdi.zaidi.etu@univ-lille.fr>
2022-10-30 18:17:32 +0100

    adding, removing distanceField & setter for categoryField in WorkingDatasets

--- src/main/java/app/graphics/models/datas/WorkingDataset.java
+++ src/main/java/app/graphics/models/datas/WorkingDataset.java
@@ -10,31 +10,43 @@ public class WorkingDataset<T extends Data> extends Dataset<T> {
 	protected String categoryField;
 	protected List<String> distanceFields;
 	protected List<KNNAlgorithm<T>> algorithms;
-	
-public WorkingDataset(String title, List<T> datas, String categoryField, List<String> distanceFields) {
+
+	public WorkingDataset(String title, List<T> datas, String categoryField, List<String> distanceFields) {
 		super(title, datas);
 		this.categoryField = categoryField;
 		this.distanceFields = distanceFields;
 		this.algorithms = new ArrayList<KNNAlgorithm<T>>();
 	}
-	
+
 	public WorkingDataset(String title, List<T> datas, String categoryField) {
 		this(title, datas, categoryField, new ArrayList<String>());
 	}
-	
+
 	public WorkingDataset(String title, List<T> datas) {
 		this(title, datas, null);
 	}
-	
+
 	public WorkingDataset(String title) {
 		this(title, new ArrayList<T>());
 	}
-	
+
 	public List<KNNAlgorithm<T>> getAlgorithms() {
 		return this.algorithms;
 	}
-	
+
 	public void addAlgorithm(KNNAlgorithm<T> algorithm) {
 		this.algorithms.add(algorithm);
+	}
+
+	public void addDistanceFieldString(String distanceFieldName) {
+		this.distanceFields.add(distanceFieldName);
+	}
+
+	public void removeDistanceFieldString(String distanceFieldName) {
+		this.distanceFields.remove(distanceFieldName);
+	}
+
+	public void setCategoryField(String categoryFieldName) {
+		this.categoryField = categoryFieldName;
 	}
 }
