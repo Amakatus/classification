3ee26b8d747131ac0e8ffb0ba026b1cd87ee4d16
Sacha Bouton <sacha.bouton.etu@univ-lille.fr>
2022-10-31 14:38:52 +0100

    test geometrycalculator removefield

--- src/test/java/app/algorithm/geometry/GeometryCalculatorTest.java
+++ src/test/java/app/algorithm/geometry/GeometryCalculatorTest.java
@@ -19,5 +19,14 @@ class GeometryCalculatorTest {
 		assertEquals("iris1",gc.getFieldsNames().get(0));
 		assertEquals(1,gc.getFieldsNames().size());
 	}
+	
+	@Test
+	void should_remove_field_in_list() {
+		List<String> fieldName = new ArrayList<>();
+		EuclideanGeometry<IrisData> gc = new EuclideanGeometry<>(fieldName);
+		gc.addField("iris1");
+		gc.removeFieldName("iris1");
+		assertEquals(0,gc.getFieldsNames().size());
+	}
 
 }
