55d049aa3ee43ae6c56bf0021b490b6391d64f0c
Amakatus <sacha.bouton.etu@univ-lille.fr>
2022-10-31 13:47:12 +0100

    test getter petal width for iris

--- src/test/java/app/graphics/models/datas/data/IrisDataTest.java
+++ src/test/java/app/graphics/models/datas/data/IrisDataTest.java
@@ -12,5 +12,12 @@ class IrisDataTest {
 		iris.setPetalLength(22.7);
 		assertEquals(22.7,iris.getPetalLength());
 	}
+	
+	@Test
+	void should_get_sepal_width() {
+		IrisData iris = new IrisData();
+		iris.setPetalWidth(3.09);
+		assertEquals(3.09,iris.getPetalWidth());
+	}
 
 }
