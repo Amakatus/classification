ed581d8890232cc90c32ff619bfda3f84c0097e0
mehdi.zaidi.etu <mehdi.zaidi.etu@univ-lille.fr>
2022-11-07 14:29:18 +0100

    merge loic test & refactor a bit

--- src/test/java/app/graphics/models/datas/WorkingDatasetTest.java
+++ src/test/java/app/graphics/models/datas/WorkingDatasetTest.java
@@ -1,9 +1,9 @@
 package app.graphics.models.datas;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+
 import app.graphics.models.datas.data.IrisData;
 
 class WorkingDatasetTest {
@@ -11,13 +11,9 @@ class WorkingDatasetTest {
 	
 	@Test
 	void Test_setter_and_getter_categoriField() {
-		IrisData wIrisOne = new IrisData();
-		IrisData wIrisTwo = new IrisData();
-		IrisData rIrisOne = new IrisData();
-		IrisData rIrisTwo = new IrisData();
-		ReferenceDataset<IrisData> referenceDS = new ReferenceDataset<IrisData>("rDS", Arrays.asList(rIrisOne, rIrisTwo));
-		WorkingDataset<IrisData> workingDS = new WorkingDataset<IrisData>("wDS", Arrays.asList(wIrisOne, wIrisTwo), referenceDS);
-		workingDS.setCategoryField("PetalLenght");
-		assertEquals("PetalLenght", workingDS.getCategoryField());
+		ReferenceDataset<IrisData> referenceDS = new ReferenceDataset<IrisData>("rDS");
+		WorkingDataset<IrisData> workingDS = new WorkingDataset<IrisData>("wDS", referenceDS);
+		workingDS.setCategoryField("petalLenght");
+		assertEquals("petalLenght", workingDS.getCategoryField());
 	}
 }
