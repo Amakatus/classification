e8ef981a28ef5b02545a115d591668b6d1715c03
Benoît <benoit.misplon.etu@univ-lille.fr>
2022-11-14 16:09:16 +0100

    Delete import

--- src/main/java/app/algorithm/normalizers/Normalizer.java
+++ src/main/java/app/algorithm/normalizers/Normalizer.java
@@ -5,8 +5,6 @@ import java.util.List;
 import java.util.Map;
 
 import app.graphics.models.datas.DataDeltas;
-import app.graphics.models.datas.ReferenceDataset;
-import app.graphics.models.datas.WorkingDataset;
 import app.graphics.models.datas.data.Data;
 import app.utils.ClassUtils;
 
@@ -15,14 +13,14 @@ public class Normalizer implements IColumnNormalizer {
 	@Override
 	public void normalize(Data data, Map<String, DataDeltas> deltas) {
 		List<Field> fields = ClassUtils.getNumberFields(data);
-		for(Field field : fields) {
+		for (Field field : fields) {
 			try {
 				double valueNotNormalized = field.getDouble(data);
 				double valueNormalized = 0;
 				DataDeltas dataDelta = deltas.get(field.getName());
 				double min = dataDelta.getMin();
 				double delta = dataDelta.getDelta();
-				valueNormalized = (valueNotNormalized - min)/delta;
+				valueNormalized = (valueNotNormalized - min) / delta;
 				field.setDouble(data, valueNormalized);
 			} catch (Exception e) {
 				e.printStackTrace();
@@ -30,19 +28,17 @@ public class Normalizer implements IColumnNormalizer {
 		}
 	}
 
-	
-
 	@Override
 	public void denormalize(Data data, Map<String, DataDeltas> deltas) {
 		List<Field> fields = ClassUtils.getNumberFields(data);
-		for(Field field : fields) {
+		for (Field field : fields) {
 			try {
 				double valueNotNormalized = field.getDouble(data);
 				double valueNormalized = 0;
 				DataDeltas dataDelta = deltas.get(field.getName());
 				double min = dataDelta.getMin();
 				double delta = dataDelta.getDelta();
-				valueNormalized = valueNotNormalized * delta + min ;
+				valueNormalized = valueNotNormalized * delta + min;
 				field.setDouble(data, valueNormalized);
 			} catch (Exception e) {
 				e.printStackTrace();
