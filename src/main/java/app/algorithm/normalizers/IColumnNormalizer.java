e6307f758dae23a6ec8169ae24e4da2a94d153b5
Benoît <benoit.misplon.etu@univ-lille.fr>
2022-11-14 16:00:40 +0100

    Denormalizer for double

--- src/main/java/app/algorithm/normalizers/IColumnNormalizer.java
+++ src/main/java/app/algorithm/normalizers/IColumnNormalizer.java
@@ -7,5 +7,5 @@ import app.graphics.models.datas.data.Data;
 
 public interface IColumnNormalizer {
 	void normalize(Data data, Map<String, DataDeltas> deltas);
-	Object denormalize(double value);
+	void denormalize(Data data, Map<String, DataDeltas> deltas);
 }
--- src/main/java/app/algorithm/normalizers/Normalizer.java
+++ src/main/java/app/algorithm/normalizers/Normalizer.java
@@ -35,9 +35,23 @@ public class Normalizer implements IColumnNormalizer {
 	
 
 	@Override
-	public Object denormalize(double value) {
-		// TODO Auto-generated method stub
-		return null;
+	public void denormalize(Data data, Map<String, DataDeltas> deltas) {
+		List<Field> fields = ClassUtils.getNumberFields(data);
+		for(Field field : fields) {
+			try {
+				double valueNotNormalized = field.getDouble(data);
+				double valueNormalized = 0;
+				DataDeltas delta = deltas.get(field.getName());
+				double min = delta.getMin();
+				double x = delta.getDelta();
+				valueNormalized = valueNotNormalized * x + min ;
+				//normalizer la data
+				field.setDouble(data, valueNormalized);
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		}
 	}
 
 }
