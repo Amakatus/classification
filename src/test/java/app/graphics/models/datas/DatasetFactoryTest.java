3b0d50c0d61dcb1a79750978f1e6cc24398008eb
Sacha Bouton <sacha.bouton.etu@univ-lille.fr>
2022-11-18 14:51:38 +0100

    refactor datasetfactory

--- src/test/java/app/graphics/models/datas/DatasetFactoryTest.java
+++ src/test/java/app/graphics/models/datas/DatasetFactoryTest.java
@@ -1,17 +1,34 @@
 package app.graphics.models.datas;
 
-import app.graphics.models.datas.data.DataType;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import app.graphics.models.datas.data.DataType;
+import app.graphics.models.datas.data.IrisData;
+import app.graphics.models.datas.data.TitanicPassengerData;
 
 class DatasetFactoryTest {
-    @Test
+	ReferenceDataset<IrisData> irisDataReferenceDS = DatasetFactory.irisReferenceDataset("iristest");
+	ReferenceDataset<TitanicPassengerData> titanicPassengerDataReferenceDS = DatasetFactory.titanicPassengerReferenceDataset("titanictest");
+    
+	@Test
     void test_create_reference_dataset_from_type() {
         // @Todo : refactor tout ça
-        assertEquals(DatasetFactory.irisReferenceDataset("test").getDatas().size(), DatasetFactory.createReferenceDataset("test", DataType.IRIS).getDatas().size());
-        assertEquals(DatasetFactory.titanicPassengerReferenceDataset("test").getDatas().size(), DatasetFactory.createReferenceDataset("test", DataType.PASSENGER).getDatas().size());
-        assertNotEquals(DatasetFactory.irisReferenceDataset("test").getDatas().size(), DatasetFactory.createReferenceDataset("test", DataType.PASSENGER).getDatas().size());
-        assertNotEquals(DatasetFactory.titanicPassengerReferenceDataset("test").getDatas().size(), DatasetFactory.createReferenceDataset("test", DataType.IRIS).getDatas().size());
-    }
+		ReferenceDataset<IrisData> factoryIrisDataDS = DatasetFactory.createReferenceDataset("test", DataType.IRIS);
+		ReferenceDataset<TitanicPassengerData> factoryTitanicPassengerDataDS = DatasetFactory.createReferenceDataset("test", DataType.PASSENGER);
+		int factoryIrisDataDSSize = factoryIrisDataDS.getDatas().size();
+		int factoryTitanicPassengerDataDSSize = factoryTitanicPassengerDataDS.getDatas().size();
+		int irisDataReferenceDSSize = irisDataReferenceDS.getDatas().size();
+		int titanicPassengerDataDSSize = titanicPassengerDataReferenceDS.getDatas().size();
+		
+		
+        assertEquals(irisDataReferenceDSSize, factoryIrisDataDSSize);
+        assertEquals(titanicPassengerDataDSSize, factoryTitanicPassengerDataDSSize);
+        assertNotEquals(irisDataReferenceDSSize, factoryTitanicPassengerDataDSSize);
+        assertNotEquals(titanicPassengerDataDSSize, factoryIrisDataDSSize);
+	}
+    
+    
 }
