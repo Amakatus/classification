d47b033e80e8c68372823fc63b278415de7e8f1e
Sacha Bouton <sacha.bouton.etu@univ-lille.fr>
2022-11-19 22:01:52 +0100

    create factory, create test for factory euclidean

--- src/main/java/app/algorithm/geometry/GeometryFactory.java
+++ src/main/java/app/algorithm/geometry/GeometryFactory.java
@@ -1,4 +1,13 @@
 package app.algorithm.geometry;
 
-public interface GeometryFactory {
+import app.algorithm.KNNCalculator;
+import app.graphics.models.datas.data.AbstractData;
+
+public interface GeometryFactory{
+	public static <T extends AbstractData> ManhatthanGeometry<T> createManhattanGeometry(KNNCalculator<T> calculator){
+		return new ManhatthanGeometry<>(calculator);
+	}
+	public static <T extends AbstractData> EuclideanGeometry<T> createEuclideanGeometry(KNNCalculator<T> calculator){
+		return new EuclideanGeometry<>(calculator);
+	}
 }
---
+++ src/test/java/app/algorithm/geometry/GeometryFactoryTest.java
@@ -0,0 +1,26 @@
+package app.algorithm.geometry;
+
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+import org.junit.jupiter.api.Test;
+
+import app.algorithm.KNNAlgorithm;
+import app.graphics.models.datas.DatasetFactory;
+import app.graphics.models.datas.WorkingDataset;
+import app.graphics.models.datas.data.DataType;
+import app.graphics.models.datas.data.IrisData;
+import app.utils.ProjectUtils;
+
+class GeometryFactoryTest {
+
+	@Test
+	void should_create_a_euclideangeometry_with_factory() {
+		 WorkingDataset<IrisData> workingData = DatasetFactory.createWorkingDataset("test", DataType.IRIS, ProjectUtils.getFile("/data/iris.csv"));
+		 workingData.createAlgorithm(2);
+		 KNNAlgorithm<IrisData> algorithm = workingData.getAlgorithms().get(0);
+		 EuclideanGeometry<IrisData> mg;
+		 mg = GeometryFactory.createEuclideanGeometry(algorithm.getCalculator());
+		 assertNotEquals(null,mg);
+	}
+
+}
