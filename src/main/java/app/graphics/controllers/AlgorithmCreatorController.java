781a527b64a08ac29b3650ffecb38984198ecc9d
Loïc Lecointe <loic.lecointe.etu>
2022-11-19 22:03:48 +0100

    pmd:fixed AlgorithmCreatorController Violation

--- src/main/java/app/graphics/controllers/AlgorithmCreatorController.java
+++ src/main/java/app/graphics/controllers/AlgorithmCreatorController.java
@@ -46,6 +46,6 @@ public class AlgorithmCreatorController extends AbstractController {
     }
 
     private boolean formIsComplete() {
-        return !(this.datasetComboBox.getSelectionModel().getSelectedItem() == null);
+        return this.datasetComboBox.getSelectionModel().getSelectedItem() != null;
     }
 }
