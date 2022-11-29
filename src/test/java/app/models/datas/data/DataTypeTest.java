4b5e7177c76100beb15c9c5a8bb2018dcb4ac2e5
Loïc Lecointe <loic.lecointe.etu@univ-lille.fr>
2022-11-29 08:54:00 +0100

    test get fields from DataType

--- src/test/java/app/models/datas/data/DataTypeTest.java
+++ src/test/java/app/models/datas/data/DataTypeTest.java
@@ -1,13 +1,27 @@
 package app.models.datas.data;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.reflect.Field;
+
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
+import app.utils.ClassUtils;
 
 class DataTypeTest {
     @Test
     void testDataTypes() {
         assertEquals(IrisData.class, DataType.IRIS.getTypeClass());
         assertEquals(TitanicPassengerData.class, DataType.PASSENGER.getTypeClass());
+    }
+    
+    @Test
+    void test_get_fields_from_enum() {
+    	Field[] enumFields = DataType.IRIS.getFields();
+    	Field[] objectFields = ClassUtils.getFields(new IrisData());
+    	assertEquals(objectFields.length, enumFields.length);
+    	for(int i = 0; i < enumFields.length; i++) {
+    		assertEquals(objectFields[i],enumFields[i]);
+    	}
     }
 }
